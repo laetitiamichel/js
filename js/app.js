7e6ecc962390831d030a742171515aa0f2ed8868
laetitiamichel <laetitia.michel@devoteam.com>
2023-09-28 17:22:15 +0200

    new file:   asset/logo.png

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", e => {
     const number = 0; // constante qu'on ne pourra jamais modifier
     elements = "javascript";
 
+    let section = document.querySelector("section");
+
     let x = 100;
     let age = 20;
     let ages = 10;
@@ -50,4 +52,35 @@ document.addEventListener("DOMContentLoaded", e => {
     };
     //pour demander la valeur d'une propriété=
     console.table(Object.values(le_nom_du_tableau));
+
+    // pour créer dynamiquement une liste UL dans le HTML via JS:
+    let user = document.createElement("ul");
+    section.appendChild(user);
+    //afficher dans la console les éléments de la section du html
+    console.log(section.children);
+    for (data in le_nom_du_tableau) {
+        console.log(`${data} : ${le_nom_du_tableau[data]}`);
+        //pour rajouter un élément dans la liste UL => innerHTML
+        // rajouter avec altgr 7 ``
+        user.innerHTML += `<li>${data} : ${le_nom_du_tableau[data]}</li>`;
+    }
+
+    //afficher un tableau d'objet avec une image:
+    let tableauObject = {
+        logo: "./asset/logo.png",
+        intro: "JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives et à ce titre est une partie essentielle des applications web. Avec les langages HTML et CSS, JavaScript est au cœur des langages utilisés par les développeurs web. Wikipédia",
+        Conçu: "Brendan Eich",
+        Date: "Mai 1996",
+        Version: "13 - ECMA-262 (Juin 2022)",
+        Développeurs: "Netscape Communications Corporation, Mozilla Foundation",
+        Extension: "js",
+        Normes: "ECMA-262; ISO/CEI 16262",
+        Paradigme: "Multi-paradigmes: script, orienté objet (orienté prototype), impératif, fonctionnel"
+
+    };
+    for (let i = 0; i < tableauObject.length; i++) {
+        console.log(`${i} : ${tableauObject[i]}`);
+    }
+    console.table(tableauObject);
+    console.table();
 });

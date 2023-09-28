9ef8b643e3988ce0c6901baa797fb000a32c907c
laetitiamichel <laetitia.michel@devoteam.com>
2023-09-28 16:21:41 +0200

    modified:   js/app.js 	modified:   src/app.js

--- js/app.js
+++ js/app.js
@@ -29,7 +29,25 @@ document.addEventListener("DOMContentLoaded", e => {
 
     //tableau:
     const tableau = ["HTML", "CSS", true];
+    let noms = ["tom", "bb", true];
+    let noms_lang = [...tableau, ...noms]; //spread opérateur = " ..." pour concaténer
     console.log(tableau.length); // length permet de compter tout le tableau
-    console.log([0]);
-    console.table(tableau);
+    //console.log([0])
+    //console.table(tableau)
+    //console.table(noms_lang)
+
+    for (let i = 0; i < noms_lang.length; i++) {
+        console.log(`${i} : ${noms_lang[i]}`);
+    }
+    // la propiété = une clé 
+    // ici => nom = propriété / clé et les : pour affecter une valeur
+    // tableau d'objet =
+    le_nom_du_tableau = {
+        nom: " lili",
+        prénom: "tim",
+        ville: "toulouse",
+        pays: "fr"
+    };
+    //pour demander la valeur d'une propriété=
+    console.table(Object.values(le_nom_du_tableau));
 });
